4ac5f7881140fa5cf637af4cfffe8487243a98c9
GeorgeAlTomeh <georgetoumeh134@gmail.com>
2026-05-11 00:31:07 +0300

    add stepper motor control with btns (uln3003)

--- sketch/thrower_ino.ino
+++ sketch/thrower_ino.ino
@@ -1,8 +1,17 @@
 #include <WiFi.h>
 #include <ESP32Servo.h>
+#include <Stepper.h>
 
 Servo myservo;
 const int servoPin = 13;
+
+// Stepper (ULN2003)
+#define STEPS_PER_REV 2048
+#define IN1 19
+#define IN2 18
+#define IN3 5
+#define IN4 15
+Stepper stepper(STEPS_PER_REV, IN1, IN3, IN2, IN4);
 
 const char* ssid = "WeaponControl";
 const char* password = "topsecret";
@@ -12,21 +21,28 @@ String header;
 String servoValue = "5";
 int pos1, pos2;
 
+int stepperStepsToMove = 0;
+int stepperSpeed = 30;
+
 void setup() {
   Serial.begin(115200);
   myservo.attach(servoPin);
+  stepper.setSpeed(stepperSpeed);
 
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+  while (WiFi.status() != WL_CONNECTED) { delay(500); Serial.print("."); }
   Serial.println("\nWiFi connected");
   Serial.println(WiFi.localIP());
   server.begin();
 }
 
 void loop() {
+  // Move stepper one step at a time if any movement queued
+  if (stepperStepsToMove != 0) {
+    stepper.step(stepperStepsToMove > 0 ? 1 : -1);
+    stepperStepsToMove += (stepperStepsToMove > 0 ? -1 : 1);
+  }
+
   WiFiClient client = server.available();
   if (client) {
     String currentLine = "";
@@ -42,14 +58,26 @@ void loop() {
             client.println("Connection: close");
             client.println();
 
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<style>.slider{width:300px;}</style>");
+            client.println("<!DOCTYPE html><html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<style>.slider{width:300px;}button{margin:5px;}</style>");
             client.println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>");
-            client.println("</head><body>");
-            client.println("<h1>ESP32 Servo Control</h1>");
+            client.println("</head><body><h1>ESP32 Motor Control</h1>");
+
+            // Servo slider
+            client.println("<h2>Servo</h2>");
             client.println("<p>Position: <span id=\"servoPos\"></span></p>");
             client.println("<input type=\"range\" min=\"0\" max=\"180\" class=\"slider\" id=\"servoSlider\" value=\"" + servoValue + "\">");
+
+            // Stepper buttons
+            client.println("<h2>Stepper</h2>");
+            client.println("<button onclick=\"stepperMove(-512)\">-1/4 Turn</button>");
+            client.println("<button onclick=\"stepperMove(-128)\">-1/16 Turn</button>");
+            client.println("<button onclick=\"stepperMove(128)\">+1/16 Turn</button>");
+            client.println("<button onclick=\"stepperMove(512)\">+1/4 Turn</button><br>");
+            client.println("<button onclick=\"stepperMove(-2048)\">-Full Turn</button>");
+            client.println("<button onclick=\"stepperMove(2048)\">+Full Turn</button><br>");
+            client.println("<button onclick=\"stepperStop()\">STOP</button>");
+
             client.println("<script>");
             client.println("$('#servoSlider').on('input', function() {");
             client.println("  var val = $(this).val();");
@@ -57,16 +85,28 @@ void loop() {
             client.println("  $.get('/?value=' + val);");
             client.println("});");
             client.println("$('#servoPos').text($('#servoSlider').val());");
-            client.println("</script>");
-            client.println("</body></html>");
+            client.println("function stepperMove(steps) { $.get('/?stepperMove=' + steps); }");
+            client.println("function stepperStop() { $.get('/?stepperStop=1'); }");
+            client.println("</script></body></html>");
 
+            // Parse requests
             if (header.indexOf("GET /?value=") >= 0) {
               pos1 = header.indexOf('=');
               pos2 = header.indexOf('&');
               servoValue = header.substring(pos1 + 1, pos2);
               myservo.write(servoValue.toInt());
-              Serial.println("Servo: " + servoValue);
             }
+            else if (header.indexOf("GET /?stepperMove=") >= 0) {
+              pos1 = header.indexOf('=');
+              pos2 = header.indexOf('&');
+              stepperStepsToMove = header.substring(pos1 + 1, pos2).toInt();
+              Serial.print("Stepper move: "); Serial.println(stepperStepsToMove);
+            }
+            else if (header.indexOf("GET /?stepperStop=1") >= 0) {
+              stepperStepsToMove = 0;
+              Serial.println("Stepper stopped");
+            }
+
             client.println();
             break;
           } else {
