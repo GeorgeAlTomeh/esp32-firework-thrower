69add5a67d5c9704050b387cbb1a9bee3f479d78
GeorgeAlTomeh <georgetoumeh134@gmail.com>
2026-05-11 00:40:52 +0300

    add stepper rpm slider

--- sketch/thrower_ino.ino
+++ sketch/thrower_ino.ino
@@ -2,10 +2,11 @@
 #include <ESP32Servo.h>
 #include <Stepper.h>
 
+// Servo
 Servo myservo;
 const int servoPin = 13;
 
-// Stepper (ULN2003)
+// Stepper (28BYJ-48 with ULN2003)
 #define STEPS_PER_REV 2048
 #define IN1 19
 #define IN2 18
@@ -13,6 +14,7 @@ const int servoPin = 13;
 #define IN4 15
 Stepper stepper(STEPS_PER_REV, IN1, IN3, IN2, IN4);
 
+// WiFi credentials – change these
 const char* ssid = "WeaponControl";
 const char* password = "topsecret";
 WiFiServer server(80);
@@ -22,7 +24,8 @@ String servoValue = "5";
 int pos1, pos2;
 
 int stepperStepsToMove = 0;
-int stepperSpeed = 30;
+int stepperSpeed = 30;          // RPM
+const int stepperMaxSpeed = 40; // 28BYJ-48 practical max
 
 void setup() {
   Serial.begin(115200);
@@ -30,14 +33,17 @@ void setup() {
   stepper.setSpeed(stepperSpeed);
 
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) { delay(500); Serial.print("."); }
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
   Serial.println("\nWiFi connected");
   Serial.println(WiFi.localIP());
   server.begin();
 }
 
 void loop() {
-  // Move stepper one step at a time if any movement queued
+  // Handle queued stepper movement (one step per loop)
   if (stepperStepsToMove != 0) {
     stepper.step(stepperStepsToMove > 0 ? 1 : -1);
     stepperStepsToMove += (stepperStepsToMove > 0 ? -1 : 1);
@@ -58,26 +64,33 @@ void loop() {
             client.println("Connection: close");
             client.println();
 
-            client.println("<!DOCTYPE html><html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<style>.slider{width:300px;}button{margin:5px;}</style>");
+            // HTML page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<style>.slider{width:300px;} button{margin:5px;}</style>");
             client.println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>");
             client.println("</head><body><h1>ESP32 Motor Control</h1>");
 
-            // Servo slider
-            client.println("<h2>Servo</h2>");
+            // Servo panel
+            client.println("<div><h2>Servo</h2>");
             client.println("<p>Position: <span id=\"servoPos\"></span></p>");
             client.println("<input type=\"range\" min=\"0\" max=\"180\" class=\"slider\" id=\"servoSlider\" value=\"" + servoValue + "\">");
+            client.println("</div>");
 
-            // Stepper buttons
-            client.println("<h2>Stepper</h2>");
+            // Stepper panel
+            client.println("<div><h2>Stepper</h2>");
             client.println("<button onclick=\"stepperMove(-512)\">-1/4 Turn</button>");
             client.println("<button onclick=\"stepperMove(-128)\">-1/16 Turn</button>");
             client.println("<button onclick=\"stepperMove(128)\">+1/16 Turn</button>");
             client.println("<button onclick=\"stepperMove(512)\">+1/4 Turn</button><br>");
             client.println("<button onclick=\"stepperMove(-2048)\">-Full Turn</button>");
             client.println("<button onclick=\"stepperMove(2048)\">+Full Turn</button><br>");
-            client.println("<button onclick=\"stepperStop()\">STOP</button>");
+            client.println("<button onclick=\"stepperStop()\">STOP</button><br><br>");
+            client.println("<p>Speed: <span id=\"stepperSpeed\">" + String(stepperSpeed) + "</span> RPM</p>");
+            client.println("<input type=\"range\" min=\"1\" max=\"" + String(stepperMaxSpeed) + "\" class=\"slider\" id=\"speedSlider\" value=\"" + String(stepperSpeed) + "\">");
+            client.println("</div>");
 
+            // JavaScript
             client.println("<script>");
             client.println("$('#servoSlider').on('input', function() {");
             client.println("  var val = $(this).val();");
@@ -87,14 +100,20 @@ void loop() {
             client.println("$('#servoPos').text($('#servoSlider').val());");
             client.println("function stepperMove(steps) { $.get('/?stepperMove=' + steps); }");
             client.println("function stepperStop() { $.get('/?stepperStop=1'); }");
+            client.println("function stepperSetSpeed(speed) {");
+            client.println("  $('#stepperSpeed').text(speed);");
+            client.println("  $.get('/?stepperSpeed=' + speed);");
+            client.println("}");
+            client.println("$('#speedSlider').on('input', function() { stepperSetSpeed($(this).val()); });");
             client.println("</script></body></html>");
 
-            // Parse requests
+            // Parse GET requests
             if (header.indexOf("GET /?value=") >= 0) {
               pos1 = header.indexOf('=');
               pos2 = header.indexOf('&');
               servoValue = header.substring(pos1 + 1, pos2);
               myservo.write(servoValue.toInt());
+              Serial.print("Servo: "); Serial.println(servoValue);
             }
             else if (header.indexOf("GET /?stepperMove=") >= 0) {
               pos1 = header.indexOf('=');
@@ -106,7 +125,13 @@ void loop() {
               stepperStepsToMove = 0;
               Serial.println("Stepper stopped");
             }
-
+            else if (header.indexOf("GET /?stepperSpeed=") >= 0) {
+              pos1 = header.indexOf('=');
+              pos2 = header.indexOf('&');
+              stepperSpeed = header.substring(pos1 + 1, pos2).toInt();
+              stepper.setSpeed(stepperSpeed);
+              Serial.print("Stepper speed: "); Serial.println(stepperSpeed);
+            }
             client.println();
             break;
           } else {
